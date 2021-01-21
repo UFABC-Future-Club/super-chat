e5fa458cf82c221fec22b82a214c252c127424fb
Matheus <matheusalexandre.sena99@gmail.com>
2021-01-21 20:36:17 -0300

    Iniciando com React Native Paper

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native'
+import { Provider as PaperProvider, DefaultTheme, TextInput, Button } from 'react-native-paper';
 
-export default function App() {
+const altura = Dimensions.get('screen').height
+
+const theme = {
+  ...DefaultTheme,
+  dark: true,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#0094FD'
+  },
+};
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider theme={theme}>
+      <View style={styles.container}>
+        <StatusBar style="auto" backgroundColor="#0094FD" />
+
+        <TextInput style={styles.textInput} mode="outlined" label="Usuário" placeholder="Digite seu usuário" />
+
+        <TextInput style={styles.textInput} mode="outlined" label="Senha" placeholder="Digite sua senha" />
+
+        <Button mode="contained" style={styles.button} onPress={() => console.log('Pressed')}>
+          Entrar
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-});
+  textInput: {
+    width: '75%',
+    height: altura * 0.075,
+    marginTop: 32,
+  },
+  button: {
+    width: '75%',
+    borderRadius: 8,
+    marginTop: 32,
+  }
+})
+
+export default App
